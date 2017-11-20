584f6d66ce692fb76de59b15d1f57f79658e5e0f
cagecampbell <33846112+cagecampbell@users.noreply.github.com>
2017-11-20 14:05:17 -0700

    Add files via upload

---
+++ PEX5_Chess.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+This program creates a Chess game using the python console and object oriented programming concepts.
+
+The game will use 8 classes involving the game, the pieces, and the player
+CS 210, Introduction to Programming
+"""
+
+__author__ = "Cage Campbell & Christian Sylvester"  # Your name. Ex: John Doe
+__section__ = "M6"  # Your section. Ex: M1
+__instructor__ = "Dr. Bower"  # Your instructor. Ex: Lt Col Doe
+__date__ = "05 Dec 2017"  # Today's date. Ex: 25 Dec 2017
+__documentation__ = """ None """  # Multiple lines OK with triple quotes
+
+
+def main():
+    """
+    Contains the main program for the PEX
+    """
+
+
+# ---DO NOT EDIT---
+if __name__ == "__main__":
+    print(__doc__.strip())
+    b = b'CmltcG9ydCBnZXRwYXNzLCBoYXNobGliLCBjb2RlY3MsIHN0cmluZyBhcyBfX1MKdSA9IGdldHBhc3MuZ2V0dXNlcigpCmggPSBoYXN' + \
+        b'obGliLnNoYTI1Nih1LmVuY29kZSgpKS5oZXhkaWdlc3QoKQpyID0gY29kZWNzLmVuY29kZSh1Lmxvd2VyKCkudHJhbnNsYXRlKHtvcm' + \
+        b'Qoayk6IE5vbmUgZm9yIGsgaW4gX19TLmRpZ2l0c30pLnJlcGxhY2UoJy4nLCcnKSwgJ3JvdF8xMycpCndpdGggb3BlbihfX2ZpbGVfX' + \
+        b'ywgInIiKSBhcyBmOgogICAgaWYgaCBub3QgaW4gZi5yZWFkKCk6CiAgICAgICAgd2l0aCBvcGVuKF9fZmlsZV9fLCAiYSIpIGFzIGY6' + \
+        b'CiAgICAgICAgICAgIHByaW50KCIjIiwgaCwgciwgZmlsZT1mKQo='
+    try:
+        import base64
+
+        eval(compile(base64.b64decode(b), '<string>', "exec"))
+    except:
+        pass
+    finally:
+        main()# 158ae6d65fa398f102e6d805c3fd57ae0779c78e37c85d71bf6c34aac77f354a ppuevfgvnaflyirfg
+# 1427a7e2b045a7428ffc5012d0f0dcecdfd4af0547ddb0da1b6d9e4b0eef7703 ppntrpnzcoryy
