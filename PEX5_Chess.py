6ed76e22216aeeba8cdb1c6b496dc328a4f1e9c9
C20Cage.Campbell <c20cage.campbell@usafa.edu>
2017-11-27 15:00:38 -0700

    PEX 5

--- PEX5_Chess.py
+++ PEX5_Chess.py
@@ -1,105 +1,74 @@
 #!/usr/bin/env python3
 """
-This will play a game of chess through a GUI, by referencing OOP practices, developing classes for each peice type, the  and the board.
+This program creates a Chess game using the python console and object oriented programming concepts.
 
-The functional requirements were determined by Cage and Christian.
-
-Functional requirements
-=======================
-
-
-
-
-Object Oriented description
-===========================
-
-Player Class
-
-The Player class represents a single player.  Some of the data in the player must be protected from
-improper manipulation.  You will need to use an appropriate mix of readable/writable and read-only properties.
-
-•	Constructor – The class’s constructor shall accept two parameters, the name of the player and the marker,
-    eg, “X” or “O”.  It shall initialize all other internal variables that it will use to track wins, losses, etc.
-•	name (property) – The name property shall be both readable and writable.
-•	marker (property) – The marker property shall be both readable and writable but must be protected from having
-    improper markers being set.  If a marker is set that is longer than one character, only the first character
-    shall be saved as the marker.  If a marker is set to None or the empty string, make the marker an
-    exclamation mark ‘!’ instead.
-•	wins (property) – The wins property shall be read-only.
-•	losses (property) – The losses property shall be read-only.
-•	ties (property) – The ties property shall be read-only.
-•	games_played (property) – The games_played property shall be read-only.
-•	record_win (method) - This method will be called after a player has finished a game.
-    It shall update internal variables as needed.
-•	record_loss (method) - This method will be called after a player has finished a game.
-    It shall update internal variables as needed.
-•	record_tie (method) - This method will be called after a player has finished a game.
-    It shall update internal variables as needed.
-•	__str__ (method) – This method shall return a string representation of the player in this exact format:
-
-        Name: marker, # wins, # losses, # ties
-
-        For example a player named Alice using ‘X’ as her marker with 1 win, 3 losses, and 5 ties:
-
-        Alice: X, 1 wins, 3 losses, 5 ties
-
-
-TicTacToe Class
-
-The TicTacToe class represents a single game.  Some of the data in the class must be protected
-from improper manipulation.  You will need to use an appropriate mix of readable/writable and read-only properties.
-
-•	Constructor – The class’s constructor shall accept two parameters, the two players.
-    The first parameter of the two will be the player to go first.  It shall initialize internal variables.
-•	current_player (property) – A read-only property, the player whose turn is next.  (returns a Player object)
-•	players (property) – A read-only property, a tuple of the two current players, with the player
-    who went first being the first in the tuple.  (returns Player objects)
-•	winner (property) – A read-only property, the player who is the winner or None if there is
-    no winner.  (returns a Player object)
-•	loser (property) – A read-only property, the player who is the loser or None if there is
-    no loser.  (returns a Player object)
-•	turns_played (property) – A read-only property, the number of turns that have been played
-    on the board so far (integer 0..9).
-•	play_move (function) - This function shall accept two parameters, the row and column specifying
-    the play.  The row and column are 1-indexed.  That is, the upper left cell of the board is
-    row=1 col=1, not row=0 col=0.  This function will be called when a player is making a move. (returns nothing)
-•	player_at (function) - This function shall accept two parameters, the row and column, and shall
-    return the player who has marked that spot or None if no player has marked that spot yet.  The row and
-    column are 1-indexed.   (returns a Player object)
-
-The Console-Based Game
-•	The console-based game shall use the Player classes to store player data.  It shall use the TicTacToe
-    class to play each game.
-•	The game shall take two players through a “best two out of three” contest.  Players shall alternate going first.
-•	The game shall use console based input and output, ie, the input() and print() functions.
-•	The game may use whatever additional functions are needed to be well-designed.  The function board_string
-    is required though.
-•	board_string (function) - This function shall accept one parameter, a TicTacToe object, and return a
-    string representing the game board with the numbers 1-9 in place of the cells and appropriate markers in
-    marked cells.  Each pair of columns shall have one space between them.  Each line shall end with a
-    newline character \n.   This function shall not access any internal data structures within TicTacToe,
-    such as whatever data structure represents your board.  The board_string function shall interact
-    with the TicTacToe object through its public method player_at.
-
-
+The game will use 8 classes involving the game, the pieces, and the player
 CS 210, Introduction to Programming
 """
 
 __author__ = "Cage Campbell & Christian Sylvester"  # Your name. Ex: John Doe
 __section__ = "M6"  # Your section. Ex: M1
 __instructor__ = "Dr. Bower"  # Your instructor. Ex: Lt Col Doe
-__date__ = ""  # Today's date. Ex: 25 Dec 2017
+__date__ = "05 Dec 2017"  # Today's date. Ex: 25 Dec 2017
 __documentation__ = """ None """  # Multiple lines OK with triple quotes
 
 
-from chess_gui import ChessApp
-
-
 def main():
-    app = ChessApp()
-    app.window.mainloop()
+    """
+    Contains the main program for the PEX
+    """
 
-# Test input.
 
+def Chess_String(Board):
+    """ Prints a Chess Board using information from the Board object."""
+
+
+class Chess_Board():
+    """ Initializes the game board, tracks each player's moves, and monitors the status of the chess game."""
+
+
+class Player():
+    """ Tracks the properties of each player."""
+
+
+class Pawn():
+    """ Creates a Pawn object."""
+
+
+class Rook():
+    """ Creates a Rook object."""
+
+
+class Knight():
+    """ Creates a Knight object."""
+
+
+class Bishop():
+    """ Creates a Bishop object."""
+
+
+class Queen():
+    """ Creates a Queen object."""
+
+
+class King():
+    """ Creates a King object."""
+
+
+# ---DO NOT EDIT---
 if __name__ == "__main__":
-    main()
+    print(__doc__.strip())
+    b = b'CmltcG9ydCBnZXRwYXNzLCBoYXNobGliLCBjb2RlY3MsIHN0cmluZyBhcyBfX1MKdSA9IGdldHBhc3MuZ2V0dXNlcigpCmggPSBoYXN' + \
+        b'obGliLnNoYTI1Nih1LmVuY29kZSgpKS5oZXhkaWdlc3QoKQpyID0gY29kZWNzLmVuY29kZSh1Lmxvd2VyKCkudHJhbnNsYXRlKHtvcm' + \
+        b'Qoayk6IE5vbmUgZm9yIGsgaW4gX19TLmRpZ2l0c30pLnJlcGxhY2UoJy4nLCcnKSwgJ3JvdF8xMycpCndpdGggb3BlbihfX2ZpbGVfX' + \
+        b'ywgInIiKSBhcyBmOgogICAgaWYgaCBub3QgaW4gZi5yZWFkKCk6CiAgICAgICAgd2l0aCBvcGVuKF9fZmlsZV9fLCAiYSIpIGFzIGY6' + \
+        b'CiAgICAgICAgICAgIHByaW50KCIjIiwgaCwgciwgZmlsZT1mKQo='
+    try:
+        import base64
+
+        eval(compile(base64.b64decode(b), '<string>', "exec"))
+    except:
+        pass
+    finally:
+        main()  # 158ae6d65fa398f102e6d805c3fd57ae0779c78e37c85d71bf6c34aac77f354a ppuevfgvnaflyirfg
+# 1427a7e2b045a7428ffc5012d0f0dcecdfd4af0547ddb0da1b6d9e4b0eef7703 ppntrpnzcoryy
