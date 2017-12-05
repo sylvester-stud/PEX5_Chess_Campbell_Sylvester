5b4282dd719fc1c26de036c2c06ab4ab114e96eb
C20Cage.Campbell <c20cage.campbell@usafa.edu>
2017-12-05 15:52:52 -0700

    PEX 5

--- PEX5_Chess.py
+++ PEX5_Chess.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 """
-This is a test
 This program creates a Chess game using the python console and object oriented programming concepts.
 
 The game will use 8 classes involving the game, the pieces, and the player
 CS 210, Introduction to Programming
 """
 
+import math
+import turtle
+
 __author__ = "Cage Campbell & Christian Sylvester"
 __section__ = "M6"
 __instructor__ = "Dr. Bower"
 __date__ = "05 Dec 2017"
 __documentation__ = """ None """
+
+"""Classes nearly complete; provisions needed for killing other pieces, and not letting pieces move through(wrap 
+   around)borders. Other things needed: GUI/Adv. features (if desired)."""
+
+# Window Properties:
+WIDTH = 800
+HEIGHT = 800
 
 
 def main():
@@ -20,51 +29,205 @@ def main():
     """
 
     player1 = Player("Name1", "White")
-    player2 = Player("Name2", "Black")
+    player2 = Player("Opponent", "Black")
     game_board = Board(player1, player2)
-    print(game_board.board)
-    print(game_board.board[9].type())
-    print(game_board.board[9].owner)
-    game_board.board[9].move()
+    # Example move: game_board.board[10].move(player1, 24, game_board, 8)
+    print(game_board.print_board())
+    Draw_Board(game_board)
+    print(game_board.board[1].type)
+    print(game_board.board[0].type)
 
 
-def chess_string(board):
-    """ Prints a Chess Board using information from the Board object."""
+def Draw_Board(Board):
+    # Attach piece images to respective file names.
+    white_pawn = "./Chess Pieces/White-Pawn.gif"
+    white_rook = "./Chess Pieces/White-Rook.gif"
+    white_knight = "./Chess Pieces/White-Knight.gif"
+    white_bishop = "./Chess Pieces/White-Bishop.gif"
+    white_queen = "./Chess Pieces/White-Queen.gif"
+    white_king = "./Chess Pieces/White-King.gif"
+    black_pawn = "./Chess Pieces/Black-Pawn.gif"
+    black_rook = "./Chess Pieces/Black-Rook.gif"
+    black_knight = "./Chess Pieces/Black-Knight.gif"
+    black_bishop = "./Chess Pieces/Black-Bishop.gif"
+    black_queen = "./Chess Pieces/Black-Queen.gif"
+    black_king = "./Chess Pieces/Black-King.gif"
+
+    window = turtle.Screen()
+    artist = turtle.Turtle()
+    piece = turtle.Turtle()
+
+    # Register all Chess Pieces in GUI.
+    window.register_shape(white_pawn)
+    window.register_shape(white_rook)
+    window.register_shape(white_bishop)
+    window.register_shape(white_knight)
+    window.register_shape(white_queen)
+    window.register_shape(white_king)
+    window.register_shape(black_pawn)
+    window.register_shape(black_rook)
+    window.register_shape(black_knight)
+    window.register_shape(black_bishop)
+    window.register_shape(black_queen)
+    window.register_shape(black_king)
+
+    window.setup(WIDTH, HEIGHT)
+    artist.speed("fastest")
+    window.tracer(0, 0)
+    color = "Black"
+    x = -WIDTH // 2
+    y = HEIGHT // 2
+    counter = 0
+    while counter < 74:
+        if counter % 9 != 0:
+            Draw_Square(artist, x, y, 100, color)
+            x += 100
+        else:
+            y -= 100
+            x = -WIDTH // 2
+        counter += 1
+        if color == "White":
+            color = "Black"
+        else:
+            color = "White"
+    window.update()
+
+    # Print pieces to board GUI.
+    for tile in Board.board:
+        artist.up()
+        location = Board.board.index(tile)
+        x = (location % 8) * 100 - 350
+        y = -(location + 1) // 8 * (HEIGHT // 8) + HEIGHT // 2 + 50
+        piece.goto(x, y)
+        if tile == ():
+            pass
+        elif tile.type == "Pawn":
+            if tile.color == "White":
+                piece.shape(white_pawn)
+            else:
+                piece.shape(black_pawn)
+        elif tile.type == "Rook":
+            if tile.color() == "White":
+                piece.shape(white_rook)
+            else:
+                piece.shape(black_rook)
+        elif tile.type == "Knight":
+            if tile.color() == "White":
+                piece.shape(white_knight)
+            else:
+                piece.shape(black_knight)
+        elif tile.type == "Bishop":
+            if tile.color() == "White":
+                piece.shape(white_bishop)
+            else:
+                piece.shape(black_bishop)
+        elif tile.type == "Queen":
+            if tile.color() == "White":
+                piece.shape(white_queen)
+            else:
+                piece.shape(black_queen)
+        else:
+            if tile.color() == "White":
+                piece.shape(white_king)
+            else:
+                piece.shape(black_king)
+
+        piece.stamp()
+    artist.down()
+
+    window.update()
+
+    mouseclick = True
+    artist.penup()
+    while mouseclick:
+        window.onscreenclick(artist.goto)
+        # artist.ht()
+        window.update()
+    window.mainloop()
+
+
+def Draw_Square(turtle, x, y, width, color):
+    turtle.seth(0)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.color(color)
+    turtle.begin_fill()
+    for i in range(4):
+        turtle.fd(width)
+        turtle.left(90)
+    turtle.end_fill()
 
 
 class Board:
     """ Initializes the game board, tracks each player's moves, and monitors the status of the chess game."""
+
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+        self.__current_player = player1
 
         # Pawn initialization
-        pawns1_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        pawns1_list = [0, 1, 2, 3, 4, 5, 6, 7]
         p1_pawns = {}
         for name in pawns1_list:
             p1_pawns[name] = Pawn(player1)
         p2_pawns = {}
         for name in pawns1_list:
             p2_pawns[name] = Pawn(player2)
-        # Rook initialization
-        rooks_list = [0, 8]
-        p1_rooks = []
-        for name in rooks_list:
-            p1_rooks.append(Rook(player1))
-        p2_rooks = []
-        for name in rooks_list:
-            p2_rooks.append(Rook(player2))
 
-        self.board = [(), (), (), (), (), (), (), (), ()] * 9
-        for i in range(9, 18):
-            self.board[i] = p1_pawns[i-9]
-        for i in range(63, 72):
-            self.board[i] = p2_pawns[i-63]
-        # Rook Placement
-        self.board[0] = p1_rooks[0]
-        self.board[8] = p1_rooks[1]
-        self.board[72] = p2_rooks[0]
-        self.board[80] = p2_rooks[1]
+        # Pawn Placement
+        self.board = [(), (), (), (), (), (), (), ()] * 8
+        for i in range(8, 16):
+            self.board[i] = p1_pawns[i - 8]
+        for i in range(48, 56):
+            self.board[i] = p2_pawns[i - 48]
+
+        # Rook Initialization/Placement
+        self.board[0] = Rook(player1)
+        self.board[7] = Rook(player1)
+        self.board[56] = Rook(player2)
+        self.board[63] = Rook(player2)
+
+        # Knight Initialization/Placement
+        self.board[1] = Knight(player1)
+        self.board[6] = Knight(player1)
+        self.board[57] = Knight(player2)
+        self.board[62] = Knight(player2)
+
+        # Bishop Initialization/Placement
+        self.board[2] = Bishop(player1)
+        self.board[5] = Bishop(player1)
+        self.board[58] = Bishop(player2)
+        self.board[61] = Bishop(player2)
+
+        # Queen Initialization/Placement
+        self.board[4] = Queen(player1)
+        self.board[59] = Queen(player2)
+
+        # King Initialization/Placement
+        self.board[3] = King(player1)
+        self.board[60] = King(player2)
+
+    @property
+    def current_player(self):
+        return self.__current_player
+
+    def new_turn(self):
+        if self.__current_player == self.player1:
+            self.__current_player = self.player2
+        else:
+            self.__current_player = self.player1
+
+    def print_board(self):
+        print(self.board[0:8])
+        print(self.board[8:16])
+        print(self.board[16:24])
+        print(self.board[24:32])
+        print(self.board[32:40])
+        print(self.board[40:48])
+        print(self.board[48:56])
+        print(self.board[56:64])
 
 
 class Player:
@@ -83,14 +246,15 @@ class Player:
     def name(self):
         return self.__name
 
+    @property
     def color(self):
         return self.__color
 
-    def switch_colors(self):
-        if self.color == "Black":
-            self.__color = "White"
-        else:
-            self.__color = "Black"
+    def wins(self):
+        return self.__wins
+
+    def losses(self):
+        return self.losses()
 
 
 class Pawn:
@@ -99,11 +263,12 @@ class Pawn:
     def __init__(self, owner):
         """
         Initializes a new Player with a name and a color.
-        :param str name: the player's name
-        :param str color: the player's color
+        :param Player owner: The player who owns the pawn.
         """
         self.__owner = owner  # type: Player
         self.__type = "Pawn"
+        self.__played = False  # tracks whether or not a pawn has moved (first move can be up to two spaces)
+        self.__color = owner.color
 
     @property
     def owner(self):
@@ -114,43 +279,107 @@ class Pawn:
         """
         return self.__owner
 
+    @property
     def type(self):
         return self.__type
 
-    def move(self, location, game, click):
-        if game.board[location] is type(Pawn):
-            if click == location + 9:
-                game.board.insert(location + 9, game.board.pop(location))
-            elif click == location + 18:
-                game.board.insert(location + 18, game.board.pop(location))
-            else:
-                pass
+    @property
+    def color(self):
+        return self.__color
+
+    def move(self, player, click, game, location):
+        """ Moves a pawn to the clicked location (rules-permitting).
+        :param Player player: The player moving.
+        :param int click: The tile number of the desired space to move to (clicked tile).
+        :param Board game: The game in which to move the pawn.
+        :param int location: The tile number of the current location.
+        """
+        pawn = game.board[location]  # type: Pawn
+        if game.current_player is player and player.color() is pawn.color and (click - location) % 8 == 0:
+            if self.__played is False and math.fabs(click - location) == 16 or player.color() == "White" and click - \
+                    location == 8 or player.color() == "Black" and location - click == 8:
+                pawn = game.board.pop(location)
+                game.board.insert(location, ())
+                game.board[click] = pawn
+                self.__played = True
+        else:
+            pass
 
 
 class Rook:
     """ Creates a Rook object."""
+
     def __init__(self, owner):
         """
-
-        :return:
+        :param Player owner: The owning player
         """
         self.__owner = owner
         self.__type = "Rook"
+        self.__color = owner.color
 
     @property
     def type(self):
         return self.__type
 
-    def __move__(self):
+    def color(self):
+        return self.__color
+
+    def move(self, location, game, click):
         """
 
         :return:
         """
-        pass
+        if (click - location) % 8 == 0 or (location % 8 - click % 8) < 8:
+            rook = game.board.pop(location)
+            game.board.insert(location, ())
+            game.board[click] = rook
+        else:
+            pass
 
 
 class Knight:
     """ Creates a Knight object."""
+
+    def __init__(self, owner):
+        """
+        Initializes a new Player with a name and a color.
+        :param Player owner: the player
+        """
+        self.__owner = owner
+        self.__type = "Knight"
+        self.__color = owner.color
+
+    @property
+    def owner(self):
+        """
+        Returns the owner
+        :return: the owner
+        :rtype: Player
+        """
+        return self.__owner
+
+    @property
+    def type(self):
+        return self.__type
+
+    def color(self):
+        return self.__color
+
+    def move(self, player, click, game, location):
+        """ Moves a pawn to the clicked location (rules-permitting).
+        :param Player player: The player moving.
+        :param int click: The tile number of the desired space to move to (clicked tile).
+        :param Board game: The game in which to move the pawn.
+        :param int location: The tile number of the current location.
+        """
+        knight = game.board[location]
+        if game.current_player is player and player.color() is knight.color:
+            if math.fabs(click - location) == 17 or math.fabs(click - location) == 15:
+                knight = game.board.pop(location)
+                game.board.insert(location, ())
+                game.board[click] = knight
+        else:
+            pass
 
 
 class Bishop:
@@ -159,11 +388,11 @@ class Bishop:
     def __init__(self, owner):
         """
         Initializes a new Player with a name and a color.
-        :param str name: the player's name
-        :param str color: the player's color
+        :param Player owner: the player who owns the bishop
         """
         self.__owner = owner  # type: Player
         self.__type = "Bishop"
+        self.__color = owner.color
 
     @property
     def owner(self):
@@ -177,15 +406,114 @@ class Bishop:
     def type(self):
         return self.__type
 
+    def color(self):
+        return self.__color
 
+    def move(self, player, click, game, location):
+        """ Moves a pawn to the clicked location (rules-permitting).
+        :param Player player: The player moving.
+        :param int click: The tile number of the desired space to move to (clicked tile).
+        :param Board game: The game in which to move the pawn.
+        :param int location: The tile number of the current location.
+        """
+        bishop = game.board[location]
+        if game.current_player is player and player.color() is bishop.color:
+            if (click - location) % 9 == 0 or (click - location) % 7 == 0:
+                bishop = game.board.pop(location)
+                game.board.insert(location, ())
+                game.board[click] = bishop
+        else:
+            pass
 
 
 class Queen:
     """ Creates a Queen object."""
 
+    def __init__(self, owner):
+        """
+        Initializes a new Player with a name and a color.
+        :param Player owner: The owning player
+        """
+        self.__owner = owner
+        self.__type = "Queen"
+        self.__color = owner.color
+
+    @property
+    def owner(self):
+        """
+        Returns the owner of the pawn
+        :return: the owner
+        :rtype: Player
+        """
+        return self.__owner
+
+    def type(self):
+        return self.__type
+
+    def color(self):
+        return self.__color
+
+    def move(self, player, click, game, location):
+        """ Moves a pawn to the clicked location (rules-permitting).
+        :param Player player: The player moving.
+        :param int click: The tile number of the desired space to move to (clicked tile).
+        :param Board game: The game in which to move the pawn.
+        :param int location: The tile number of the current location.
+        """
+        queen = game.board[location]
+        if game.current_player is player and player.color() is queen.color:
+            if (click - location) % 8 == 0 or (location % 8 - click % 8) < 8 or (click - location) % 9 == 0 or (
+                        click - location) % 7 == 0:
+                queen = game.board.pop(location)
+                game.board.insert(location, ())
+                game.board[click] = queen
+        else:
+            pass
+
 
 class King:
     """ Creates a King object."""
+
+    def __init__(self, owner):
+        """
+        Initializes a new Player with a name and a color.
+        :param Player owner: The owning Player
+        """
+        self.__owner = owner  # type: Player
+        self.__type = "King"
+        self.__color = owner.color
+
+    @property
+    def owner(self):
+        """
+        Returns the owner of the pawn
+        :return: the owner
+        :rtype: Player
+        """
+        return self.__owner
+
+    def type(self):
+        return self.__type
+
+    def color(self):
+        return self.__color
+
+    def move(self, player, click, game, location):
+        """ Moves a pawn to the clicked location (rules-permitting).
+        :param Player player: The player moving.
+        :param int click: The tile number of the desired space to move to (clicked tile).
+        :param Board game: The game in which to move the pawn.
+        :param int location: The tile number of the current location.
+        """
+        king = game.board[location]
+        if game.current_player is player and player.color() is king.color:
+            if math.fabs(click - location) == 1 or math.fabs(click - location) == 7 or math.fabs(
+                            click - location) == 8 or math.fabs(click - location) == 9:
+                king = game.board.pop(location)
+                game.board.insert(location, ())
+                game.board[click] = king
+        else:
+            pass
 
 
 # ---DO NOT EDIT---
